481e2200a2a16a684cfc65ced266a6642b3d41b5
Víctor Domínguez <viconel27@gmail.com>
2024-05-04 22:51:10 +0200

    Easy dependencies upgrade
    Signed-off-by: Víctor Domínguez <viconel27@gmail.com>

--- app/src/main/java/es/upm/bienestaremocional/MainApplication.kt
+++ app/src/main/java/es/upm/bienestaremocional/MainApplication.kt
@@ -16,8 +16,11 @@ class MainApplication : Application(), Configuration.Provider {
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
 
-    override fun getWorkManagerConfiguration() =
-        Configuration.Builder()
+    /**
+     * The [Configuration] used to initialize WorkManager
+     */
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
             .setWorkerFactory(workerFactory)
             .build()
 }
